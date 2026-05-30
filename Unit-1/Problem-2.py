df0a772dbdc59b3e7ee2fefe3dfc99004eeb5397
Thierry <151109030+DeveloperThierry@users.noreply.github.com>
2026-05-30 12:07:05 -0400

    Create Problem-2.py

---
+++ Unit-1/Problem-2.py
@@ -0,0 +1,19 @@
+# Tech Fellow Task Instructions 
+For each problem in your assigned task, write out the UPI solution by answering questions 1-4.
+
+# Template Questions
+### U - Understand 
+1. Share 2 questions you would ask to help understand the question:
+  <!-- Your answer here -->
+
+### P - Plan
+
+2. Write out in plain English what you want to do: 
+  <!-- Your answer here -->
+3. Translate each sub-problem into pseudocode:
+  <!-- Your answer here -->
+
+
+### I - Implement
+4. Translate the pseudocode into Python and share your final answer:
+  <!-- Your answer here -->
