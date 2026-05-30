3fbd67560a5558eb125a855bcf04c65cfd7cb602
Thierry <151109030+DeveloperThierry@users.noreply.github.com>
2026-05-30 12:44:33 -0400

    Update Problem-2.py

--- Unit-1/Version-1/Problem-2.py
+++ Unit-1/Version-1/Problem-2.py
@@ -1,19 +1,30 @@
 # Tech Fellow Task Instructions 
-For each problem in your assigned task, write out the UPI solution by answering questions 1-4.
+#For each problem in your assigned task, write out the UPI solution by answering questions 1-4.
 
 # Template Questions
 ### U - Understand 
-1. Share 2 questions you would ask to help understand the question:
-  <!-- Your answer here -->
-
+#1. Share 2 questions you would ask to help understand the question:
+#Should we hardcode in the mood?
+#Can we use text instead of emojis
+    
 ### P - Plan
-
-2. Write out in plain English what you want to do: 
-  <!-- Your answer here -->
-3. Translate each sub-problem into pseudocode:
-  <!-- Your answer here -->
-
+#2. Write out in plain English what you want to do: 
+#Open the function todays_mood
+#Set the mood
+#Change the mood assignment
+#Print the mood using concatenation
+#Call the Function at the end
+    
+#3. Translate each sub-problem into pseudocode:
+#Define function todays_mood
+    #set the mood to emoji or text
+    #print today's mood with concatenation
+#Call todays_mood
 
 ### I - Implement
-4. Translate the pseudocode into Python and share your final answer:
-  <!-- Your answer here -->
+#4. Translate the pseudocode into Python and share your final answer:
+def todays_mood():
+    mood =  "🥱"
+    print("Today's mood: " + mood)
+
+todays_mood()
