a927a0fa017aca5cc494c69e715d03c6abf69372
Thierry <151109030+DeveloperThierry@users.noreply.github.com>
2026-05-30 12:36:13 -0400

    Update Problem-1.py

--- Unit-1/Version-1/Problem-1.py
+++ Unit-1/Version-1/Problem-1.py
@@ -1,19 +1,25 @@
 # Tech Fellow Task Instructions 
-For each problem in your assigned task, write out the UPI solution by answering questions 1-4.
+#For each problem in your assigned task, write out the UPI solution by answering questions 1-4.
 
 # Template Questions
 ### U - Understand 
-1. Share 2 questions you would ask to help understand the question:
-  <!-- Your answer here -->
+#1. Should the function always be declared above its usage
+#2. Should the contents inside function be indented?
 
 ### P - Plan
+#2. Write out in plain English what you want to do: 
+    #1. Define function using def and name the function 
+    #2. Indent and place print Hello World statement
+    #3. Call the function by writing its name outside function with parenthesis
 
-2. Write out in plain English what you want to do: 
-  <!-- Your answer here -->
-3. Translate each sub-problem into pseudocode:
-  <!-- Your answer here -->
+#3. Translate each sub-problem into pseudocode:
+    #1. Define a function named hello world
+        #2. print hello world
+    #3. Call the Hello World function
 
 
 ### I - Implement
-4. Translate the pseudocode into Python and share your final answer:
-  <!-- Your answer here -->
+#4. Translate the pseudocode into Python and share your final answer:
+def hello_world():
+    print("Hello world!")
+hello_world()
