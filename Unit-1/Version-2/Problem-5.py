348591cb1da7bfc0a87737958a936934a055033b
Thierry <151109030+DeveloperThierry@users.noreply.github.com>
2026-05-30 13:41:20 -0400

    Update Problem-5.py

--- Unit-1/Version-2/Problem-5.py
+++ Unit-1/Version-2/Problem-5.py
@@ -1,19 +1,29 @@
 # Tech Fellow Task Instructions 
-For each problem in your assigned task, write out the UPI solution by answering questions 1-4.
+#For each problem in your assigned task, write out the UPI solution by answering questions 1-4.
 
 # Template Questions
 ### U - Understand 
-1. Share 2 questions you would ask to help understand the question:
-  <!-- Your answer here -->
+#1. Share 2 questions you would ask to help understand the question:
+#What should be returned if list is empty?
+#Can the list contain negative numbers?
 
 ### P - Plan
 
-2. Write out in plain English what you want to do: 
-  <!-- Your answer here -->
-3. Translate each sub-problem into pseudocode:
-  <!-- Your answer here -->
+#2. Write out in plain English what you want to do: 
+#Find the biggest and smallest number in the array and return the difference.
 
+#3. Translate each sub-problem into pseudocode:
+#if list is empty return 0
+#if set max value equals max of list 
+#if set min value equals min of list 
+#return max value minus min value
 
 ### I - Implement
-4. Translate the pseudocode into Python and share your final answer:
-  <!-- Your answer here -->
+#4. Translate the pseudocode into Python and share your final answer:
+def max_difference(lst):
+    mx=max(lst)
+    mn=min(lst)
+    return mx-mn
+lst = [5,22,8,10,2]
+max_diff = max_difference(lst)
+print(max_diff)
