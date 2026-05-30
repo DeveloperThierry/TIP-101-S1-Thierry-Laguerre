b9fde048e0bf8b88e8d6993e539e19491df00ca4
Thierry <151109030+DeveloperThierry@users.noreply.github.com>
2026-05-30 12:54:03 -0400

    Update Problem-3.py

--- Unit-1/Version-1/Problem-3.py
+++ Unit-1/Version-1/Problem-3.py
@@ -1,19 +1,23 @@
 # Tech Fellow Task Instructions 
-For each problem in your assigned task, write out the UPI solution by answering questions 1-4.
+#For each problem in your assigned task, write out the UPI solution by answering questions 1-4.
 
 # Template Questions
 ### U - Understand 
-1. Share 2 questions you would ask to help understand the question:
-  <!-- Your answer here -->
+#1. Share 2 questions you would ask to help understand the question:
+#Can I pass a string emoji as params or should I place it in variable?
+#Should we consider rendering it dynamically in case of different menu items.
 
 ### P - Plan
+#2. Write out in plain English what you want to do: 
+#I want to call print_menu with a specific emoji string and have it concatenated in a string and printed
 
-2. Write out in plain English what you want to do: 
-  <!-- Your answer here -->
-3. Translate each sub-problem into pseudocode:
-  <!-- Your answer here -->
-
+#3. Translate each sub-problem into pseudocode:
+#function print_menu(menu):
+    #print lunch is menu
+#call print_menu with argument pizza
 
 ### I - Implement
-4. Translate the pseudocode into Python and share your final answer:
-  <!-- Your answer here -->
+#4. Translate the pseudocode into Python and share your final answer:
+def print_menu(menu):
+    print("Lunch today is: " + menu)
+print_menu("🍕")
